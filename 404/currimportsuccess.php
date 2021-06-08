08b3249af181069be7df5524f8cf4beedfc653ab
Zelkey <69836819+ZelkeyZZ@users.noreply.github.com>
2021-06-08 13:47:56 +0800

    Add files via upload

---
+++ 404/currimportsuccess.php
@@ -0,0 +1,51 @@
+<!DOCTYPE html>
+<html lang="en">
+	<head>
+		<meta charset="UTF-8">
+		<meta name="viewport" content="width=device-width, initial-scale=3, user-scalable=yes" />
+		<title>Success</title>
+		<link href="../assets/css/bootstrap.min.css" rel="stylesheet" media="all">
+		<link href="../assets/css/main.css" rel="stylesheet" />
+			<noscript>
+				<link href="../assets/css/noscript.css" rel="stylesheet" />
+			</noscript>
+		<link rel="shortcut icon" type="image/png" href="../images/icon/favicon.png"/>
+		
+		<style>
+			.wrapper{
+				width: 600px;
+				margin: 0 auto;
+			}
+		</style>
+	</head>
+	<body class="landing is-preload">
+		<div class="page-wrapper">
+			<section id="banner">
+				<div class="inner">
+					<h2> Request Granted </h2>
+					<p>  Successfully Imported Subject Curriculum. </p>
+					<ul class="actions special">
+						<li><a href="../curriculum/addfile" class="button primary">Import Another Subject</a></li>
+						<li><a href="../curriculum/list" class="button primary">Return</a></li>
+					</ul>
+				</div>
+			</section>
+			<footer id="footer">
+				<ul class="copyright">
+					<li>Copyright &copy; Informatics College</li>
+					<li>Design & Template: <a href="#">ZKArts</a></li>
+				</ul>
+			</footer>
+		</div>
+	
+	<!-- Scripts -->
+	<script src="../assets/js/jquery.min.js"></script>
+	<script src="../assets/js/jquery.scrollex.min.js"></script>
+	<script src="../assets/js/jquery.scrolly.min.js"></script>
+	<script src="../assets/js/browser.min.js"></script>
+	<script src="../assets/js/breakpoints.min.js"></script>
+	<script src="../assets/js/util.js"></script>
+	<script src="../assets/js/main.js"></script>
+	
+	</body>
+</html>
---
+++ 404/cview404.php
@@ -0,0 +1,50 @@
+<!DOCTYPE html>
+<html lang="en">
+	<head>
+		<meta charset="UTF-8">
+		<meta name="viewport" content="width=device-width, initial-scale=3, user-scalable=yes" />
+		<title>404</title>
+		<link href="../assets/css/bootstrap.min.css" rel="stylesheet" media="all">
+		<link href="../assets/css/main.css" rel="stylesheet" />
+			<noscript>
+				<link href="../assets/css/noscript.css" rel="stylesheet" />
+			</noscript>
+		<link rel="shortcut icon" type="image/png" href="../images/icon/favicon.png"/>
+		
+		<style>
+			.wrapper{
+				width: 600px;
+				margin: 0 auto;
+			}
+		</style>
+	</head>
+	<body class="landing is-preload">
+		<div class="page-wrapper">
+			<section id="banner">
+				<div class="inner">
+					<h2>Invalid Request</h2>
+					<p> 404 Not Found </p>
+					<ul class="actions special">
+						<li><a href="../clearance/list" class="button primary">Return</a></li>
+					</ul>
+				</div>
+			</section>
+			<footer id="footer">
+				<ul class="copyright">
+					<li>Copyright &copy; Informatics College</li>
+					<li>Design & Template: <a href="#">ZKArts</a></li>
+				</ul>
+			</footer>
+		</div>
+	
+	<!-- Scripts -->
+	<script src="../assets/js/jquery.min.js"></script>
+	<script src="../assets/js/jquery.scrollex.min.js"></script>
+	<script src="../assets/js/jquery.scrolly.min.js"></script>
+	<script src="../assets/js/browser.min.js"></script>
+	<script src="../assets/js/breakpoints.min.js"></script>
+	<script src="../assets/js/util.js"></script>
+	<script src="../assets/js/main.js"></script>
+	
+	</body>
+</html>
